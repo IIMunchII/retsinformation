f4d27c5bc05d4a6d88aa70c04e79c53518cbb888
Jonas Høgh Jeppesen <jonash@email.fo>
2021-05-03 19:41:53 +0200

    tilføjet django app til at lagring af resultater i en database.

--- retsinformation/items.py
+++ retsinformation/items.py
@@ -4,7 +4,7 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from scrapy_djangoitem import DjangoItem
 
 class RetsinformationItem(scrapy.Item):
     # define the fields for your item here like:
