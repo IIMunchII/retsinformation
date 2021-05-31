6223d2f2de03dac6fe31153ed1ce167a22573fa3
Jonas Høgh Jeppesen <jha@peachestheapp.com>
2021-05-31 22:06:13 +0200

    ændret framework for kontrol med systemvariable ved deployment. Vi bruger nu django-environ.

--- retsinfo_app/retsinfo_app/settings/settings.py
+++ retsinfo_app/retsinfo_app/settings/settings.py
@@ -11,15 +11,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9-7+hmwtb%kc6c^zjbq9a#^7uy@++js-_l2a9tqd)%$+ut%k)f'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
     'scrapers.apps.ScrapersConfig',
     'documents.apps.DocumentsConfig',
-    'djecrety',
 ]
 
 MIDDLEWARE = [
